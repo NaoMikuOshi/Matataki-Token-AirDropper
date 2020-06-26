6ee385f7e0e2668d0dc5d9d81ed0cb384127ad55
Nao KM <NaoMikuOshi@outlook.com>
2020-06-26 15:37:39 +0800

    feat: backend API

---
+++ src/api/backend.js
@@ -0,0 +1,114 @@
+import axios from "axios";
+
+const client = axios.create({
+  baseURL: process.env.REACT_APP_BACKEND_API,
+  timeout: 1000 * 30,
+  headers: {},
+});
+
+// Just copy from matataki-fe
+client.interceptors.request.use(
+  (config) => {
+    if (getCookie("ACCESS_TOKEN"))
+      config.headers["x-access-token"] = getCookie("ACCESS_TOKEN");
+    return config;
+  },
+  (error) => {
+    // Do something with request error
+    return Promise.reject(error);
+  }
+);
+
+client.interceptors.response.use(
+  (response) => {
+    // if(loadingInstance) loadingInstance.close();
+    return response.data;
+  },
+  (error) => {
+    // loadingInstance.close()
+    console.log(error.message);
+
+    if (error.message.includes("status code 401")) {
+      alert("登录状态异常,请重新登录");
+    }
+    //     if (process.browser && window && window.$nuxt) {
+
+    //       // window.$nuxt.$store.commit('setLoginModal', true)
+
+    //       try {
+    //         // 重置all store
+    //         window.$nuxt.$store.dispatch('resetAllStore')
+    //         .then(() => {
+    //           clearAllCookie()
+    //           // 防止没有清除干净
+    //           removeCookie('ACCESS_TOKEN')
+    //           removeCookie('idProvider')
+    //           store.clearAll()
+    //           sessionStorage.clear()
+    //         }).catch(err => {
+    //           console.log(err)
+    //           removeCookie('ACCESS_TOKEN')
+    //         })
+    //       } catch(e) {
+    //         console.log(e)
+    //         removeCookie('ACCESS_TOKEN')
+    //       }
+    //     }
+    //   }
+
+    // 超时处理
+    if (error.message.includes("timeout")) {
+      // Message.closeAll()
+      alert("请求超时");
+    }
+    if (error.message.includes("Network Error")) {
+      alert("Network Error");
+    }
+    // loadingInstance.close()
+    return Promise.reject(error);
+  }
+);
+
+/**
+ * Api Docs in over here: https://docs.matataki.io/airdrop.html
+ */
+
+/**
+ *
+ * @param {string} title 标题
+ * @param {number} tokenId fan票id
+ * @param {number} amount 空投总量
+ * @param {number} quantity 空投份额
+ * @param {number} duration 持续时间（单位：天）
+ */
+export function createAirdrop(title, tokenId, amount, quantity, duration) {
+  return client.post("/airdrop", {
+    title,
+    tokenId,
+    amount,
+    quantity,
+    duration,
+  });
+}
+
+export function getAirdropList(page = 1, limit = 10, sort = "created_at,DESC") {
+  return client.get("/airdrop", {
+    params: {
+      sort,
+      limit,
+      page,
+    },
+  });
+}
+
+export function getDetailOfAirdrop(cashtag) {
+  return client.get(`/airdrop/${cashtag}`);
+}
+
+/**
+ * Claim Airdrop
+ * @param {string} cashtag The $Cashtag of the airdrop
+ */
+export function claimAirdrop(cashtag, memo = "") {
+  return client.post(`/airdrop/${cashtag}`, { memo });
+}
