20f072ffe674bee85bf6e32df7c0a78b9320ac7f
Nao KM <NaoMikuOshi@outlook.com>
2020-07-02 15:51:46 +0800

    feat: getTokenList

--- src/api/token.js
+++ src/api/token.js
@@ -3,3 +3,12 @@ import axios from "./index";
 export function getTokenProfile(id) {
   return axios.get(`/minetoken/${id}`);
 }
+
+export function getTokenList() {
+  return axios.get("/token/tokenlist", {
+    params: {
+      pagesize: 999,
+      order: 0,
+    },
+  });
+}
