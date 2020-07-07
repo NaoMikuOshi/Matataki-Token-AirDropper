896a57b9a86b88f222da367c4ec045ebb561d337
Nao KM <NaoMikuOshi@outlook.com>
2020-07-07 14:13:48 +0800

    feat: MyPage

--- src/pages/MyAirdrop.jsx
+++ src/pages/MyAirdrop.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { uniq, map, compose } from "ramda";
 import { Link } from "react-router-dom";
 import { Table, Panel } from "react-bulma-components";
@@ -16,7 +16,7 @@ export function MyAirdrop() {
     const tokenIds = compose(
       uniq,
       map((e) => e.token_id)
-    )(events);
+    )([...events, ...logs]);
     const tokens = await Promise.all(
       tokenIds.map(async (id) => {
         const { data } = await getTokenProfile(id);
@@ -30,6 +30,7 @@ export function MyAirdrop() {
     return { events, logs, tokenIds, tokenProfile };
   }
   const { data, loading, error } = useRequest(() => fetchData());
+  const [currentTab, updateTab] = useState("airdrop");
   if (loading) {
     return (
       <div
@@ -53,13 +54,79 @@ export function MyAirdrop() {
     <Panel style={{ maxWidth: "650px", margin: "10px auto" }} color="primary">
       <Panel.Header>My page</Panel.Header>
       <Panel.Tabs className="panel-tabs">
-        <Panel.Tabs.Tab active>My AirDrop</Panel.Tabs.Tab>
-        <Panel.Tabs.Tab>My Claims</Panel.Tabs.Tab>
+        <Panel.Tabs.Tab
+          active={currentTab === "airdrop"}
+          onClick={() => updateTab("airdrop")}
+        >
+          My AirDrop
+        </Panel.Tabs.Tab>
+        <Panel.Tabs.Tab
+          active={currentTab === "claims"}
+          onClick={() => updateTab("claims")}
+        >
+          My Claims
+        </Panel.Tabs.Tab>
       </Panel.Tabs>
       <Panel.Block className="panel-block">
-        <EventsList events={data.events} tokenProfile={data.tokenProfile} />
+        {currentTab === "airdrop" && (
+          <EventsList events={data.events} tokenProfile={data.tokenProfile} />
+        )}
+        {currentTab === "claims" && (
+          <ClaimLogs events={data.logs} tokenProfile={data.tokenProfile} />
+        )}
       </Panel.Block>
     </Panel>
+  );
+}
+
+function ClaimLogs({ events, tokenProfile }) {
+  return (
+    <div className="events">
+      <Table>
+        <thead>
+          <tr>
+            <th>Cashtag</th>
+            <th>Amount</th>
+            <th>Date</th>
+            <th>Status</th>
+          </tr>
+        </thead>
+        <tfoot>
+          <tr>
+            <th>Cashtag</th>
+            <th>Amount</th>
+            <th>Date</th>
+            <th>Status</th>
+          </tr>
+        </tfoot>
+        <tbody>
+          {events.map((event) => (
+            <ClaimLog
+              key={event.id}
+              event={event}
+              token={tokenProfile[event.token_id]}
+            />
+          ))}
+        </tbody>
+      </Table>
+    </div>
+  );
+}
+
+function ClaimLog({ event, token }) {
+  return (
+    <tr>
+      <td>
+        <Link to={`/claim/${event.cashtag}`} target="_blank">
+          ${event.cashtag}
+        </Link>
+      </td>
+      <td>
+        {event.amount / 10 ** token.decimals} {token.symbol}
+      </td>
+      <td>{new Date(event.created_at).toLocaleString()}</td>
+      <td>{event.status}</td>
+    </tr>
   );
 }
 
